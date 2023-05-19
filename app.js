f118ea0ad3be36df3495d7517c9b7c955ff9a702
Dan Todd <devdantodd@proton.me>
2023-05-19 12:56:28 -0400

    0.2

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
+// Function to summarize the web page using ChatGPT
 function summarizePage() {
-	var url = document.getElementById("url").value;
-	if (url) {
-		fetch("https://api.smmry.com/?SM_API_KEY=5309BEE48E&SM_URL=" + url)
-			.then(response => response.json())
-			.then(data => {
-				if (data.sm_api_error) {
-					alert(data.sm_api_error);
-				} else {
-					document.getElementById("summary").innerHTML = "<h2>" + data.sm_api_title + "</h2>" + data.sm_api_content + "<img src='" + data.sm_api_image_url + "' alt='Summary Image'>";
-					document.getElementById("summary").style.display = "block";
-				}
-			})
-			.catch(error => alert("An error occurred while summarizing the page."));
-	} else {
-		alert("Please enter a URL.");
-	}
+    const url = document.getElementById("url").value;
+
+    if (url) {
+        axios.post('/summarize', { url })
+            .then(response => {
+                const { summary, image_url } = response.data;
+                document.getElementById("summary").innerHTML = `<h2>${summary}</h2><img src="${image_url}" alt="Summary Image">`;
+                document.getElementById("summary").style.display = "block";
+            })
+            .catch(error => {
+                alert("An error occurred while summarizing the page.");
+            });
+    } else {
+        alert("Please enter a URL.");
+    }
 }
